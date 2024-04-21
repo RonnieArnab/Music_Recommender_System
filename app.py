73d913ad5a57ed50e73d4168e71080e3eb111bd8
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2024-04-21 21:25:48 +0530

    add frontend

--- app.py
+++ app.py
@@ -1,28 +1,61 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template
+from flask import request as req
 
-from utils import utils
+from service import music_service
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(
+    __name__,
+    static_url_path="",
+    static_folder="web/static",
+    template_folder="web/templates",
+)
+
+playlist: list[music_service.Song] = list()
+
+
+@app.route("/")
+def index():
+    playlist.clear()
+    return render_template("pages/index.html")
+
+
+@app.route("/search", methods=["POST"])
+def search_songs():
+    query = req.form["query"]
+    if not query or len(query) < 2:
+        return render_template(
+            "partials/error.html", msg="At least enter 2 characters to search songs"
+        )
+
+    # get music searches
+    search_results = music_service.search_song(query, limit=50)
+
+    return render_template("partials/search-response.html", results=search_results)
+
+
+@app.route("/add-to-playlist", methods=["POST"])
+def add_to_playlist():
+    name = req.form["name"]
+    year = int(req.form["year"])
+
+    playlist.append(music_service.Song(name=name, release_year=year, artists=[]))
+    return render_template("partials/playlist.html", playlist=playlist)
 
 
 @app.route("/recommend", methods=["POST"])
-def get_recommended_songs():
-    data = request.get_json()
+def recommend_songs():
+    song_names = req.form.getlist("name")
+    song_release_years = req.form.getlist("year")
 
-    if "songs" not in data:
-        return jsonify({"error": "No 'songs' key found in the request."}), 400
+    playlist_songs = [
+        music_service.Song(name=name, release_year=int(year), artists=[])
+        for name, year in zip(song_names, song_release_years)
+    ]
 
-    songs = data["songs"]
+    recommedations = music_service.recommend_songs(playlist_songs)
 
-    if not isinstance(songs, list):
-        return jsonify({"error": "'songs' must be a list of song IDs."}), 400
-
-    recommended_songs = utils.recommend_songs(songs)
-
-    return jsonify({"recommended_songs": recommended_songs})
-
-
-if __name__ == "__main__":
-    app.run(debug=False)
+    return render_template(
+        "partials/recommendations.html",
+        recommendations=recommedations,
+        playlist=playlist_songs,
+    )
